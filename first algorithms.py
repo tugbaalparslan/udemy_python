14f21ee9173f14fe985856c12d3cade9c2979d14
tugbaalparslan <tugba.alparslan@outlook.com>
2019-09-23 23:51:09 -0700

    first

--- first algorithms.py
+++ first algorithms.py
@@ -1,34 +1,29 @@
-num_array = [15,8,98,23,7,1,67,90,56,3,88,101]
-
-# print(num_array)
-
-
-# we are gonna find the min item of the list
-
 import sys
-min = sys.maxsize
-
-for x in num_array:
-    if x < min:
-        min = x
 
 
-print(f"Min num int he list is: {min}")
+def get_min_from_array(int_array):  # we are gonna find the min item of a given list
+    min = sys.maxsize
+
+    for x in int_array:
+        if x < min:
+            min = x
+
+    print(f"Min number in the list is: {min}")
 
 
-# we are gonna find the second min number in a given list
+def get_min2_from_array(int_array):  # we are gonna find the second min number in a given list
+    min1 = min2 = sys.maxsize
+
+    for x in int_array:
+        if x < min1:
+            min2 = min1
+            min1 = x
+        elif x < min2:
+            min2 = x
+
+    print(f"Min number in the list is: {min1}, Second min number in the list is: {min2}")
 
 
-min1 = sys.maxsize -1
-min2 = sys.maxsize
-
-
-for x in num_array:
-    if x < min1:
-        min2 = min1
-        min1 = x
-    elif x < min2:
-        min2 = x
-
-print(f"Min1 = {min1}, Min2 = {min2}")
-
+num_array = [15, 8, 98, 23, 7, 1, 67, 90, 56, 3, 88, 101]
+get_min_from_array(num_array)
+get_min2_from_array(num_array)
